fb8d1d253399ac974aa7250cd498cb9474fd17f2
Eugene Sazonov <saz.vit@gmail.com>
2022-08-11 23:29:19 +0200

    Initial template project state

---
+++ src/main/java/org/zzy/springsecuritydemo/controllers/DevelopersControllerV1.java
@@ -0,0 +1,31 @@
+package org.zzy.springsecuritydemo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.zzy.springsecuritydemo.model.Developer;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RestController
+@RequestMapping("/api/v1/developers")
+public class DevelopersControllerV1 {
+	private List<Developer> developers = Stream.of(
+			new Developer(1L, "Ivan", "Ivanov"),
+			new Developer(2L, "Sergey", "Sergeyev"),
+			new Developer(3L, "Petr", "Petrov")
+	).collect(Collectors.toList());
+
+	@GetMapping
+	public List<Developer> getAll() {
+		return developers;
+	}
+
+	@GetMapping("/{id}")
+	public Developer getById(@PathVariable Long id) {
+		return developers.stream().filter(developer -> id.equals(developer.getId())).findFirst().orElse(null);
+	}
+}
